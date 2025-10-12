fc67705abadaea025ba5127f78a940f5dbd07539
ikram000 <ikramjaved857@gmail.com>
2025-10-12 07:35:20 -0700

    Basic module knowledge

--- Lecture_2_Modules/index.js
+++ Lecture_2_Modules/index.js
@@ -1,6 +1,6 @@
 //console.log("we are learning about the module in node.js")
 
-// const { sum, sub, mul, div } = require('./utils')
+// const { sum, sub, mul, div } = require('./utils');
 import {sum,sub,mul,div} from './utils.js'
 console.log("Sum of two numbers =", sum(1, 20));
 console.log("Multiplication of two numbers =", mul(15, 20));
